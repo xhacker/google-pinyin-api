fa3ec599b25d743afde1a9c9e8db7646fe6e0a91
Yichen Lu <wz.bluesnow@gmail.com>
2009-11-13 20:39:52 +0800

    ???????

--- quhao.lua
+++ quhao.lua
@@ -3,6 +3,7 @@
 ------------------------------------------------
 -- 谷歌拼音区号查询扩展
 -- 版本: 0.1 unfinished
+-- 作者: Liu Dongyuan
 -- 项目主页: http://code.google.com/p/google-pinyin-api/
 --
 -- 简介: 查询区号（暂时只支持国内）
