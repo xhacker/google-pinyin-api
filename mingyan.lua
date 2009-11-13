3a56bd57478dd451bda482592be5909dbd6c9332
wz bluesnow <wz.bluesnow@gmail.com>
2009-11-13 19:30:08 +0800

    ????????????????google-pinyin-api?

--- mingyan.lua
+++ mingyan.lua
@@ -3,7 +3,7 @@
 ------------------------------------------------
 -- 谷歌拼音名人语录扩展
 -- 版本: 0.2 pre
--- 项目主页: http://code.google.com/p/googlepinyin-plugins/
+-- 项目主页: http://code.google.com/p/google-pinyin-api/
 --
 -- 简介: 随机打出一些名人说过的话。
 --
