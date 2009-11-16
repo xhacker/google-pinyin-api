9fcc14f8620c6a1a2b27c699208852a574f98d03
Yichen Lu <wz.bluesnow@gmail.com>
2009-11-16 22:35:37 +0800

    ?? Mike ???????????????? Pingji ??????

--- pingji.lua
+++ pingji.lua
@@ -1,4 +1,15 @@
-_SYMBOL_TABLE={
+-- encoding: UTF-8
+
+------------------------------------------------
+-- 谷歌拼音评级符号扩展
+-- 作者: Mike
+-- 版本: 0.1
+-- 项目主页: http://code.google.com/p/google-pinyin-api/
+--
+-- 此扩展遵循GPLv3发布
+------------------------------------------------
+
+_Pingji_TABLE={
 ["xx"] = { "★☆", {"★☆☆☆☆ ","★★☆☆☆ ","★★★☆☆ ","★★★★☆ ","★★★★★ ","★★★★★★☆☆☆☆","★★★★★★★☆☆☆","★★★★★★★★☆☆","★★★★★★★★★☆","★★★★★★★★★★"} },
 ["fk"] = { "■□", {"■□□□□","■■□□□","■■■□□","■■■■□","■■■■■","■■■■■■□□□□","■■■■■■■□□□","■■■■■■■■□□","■■■■■■■■■□","■■■■■■■■■■"} },
 ["sj"] = { "▲△", {"▲△△△△","▲▲△△△","▲▲▲△△","▲▲▲▲△","▲▲▲▲▲","▲▲▲▲▲△△△△","▲▲▲▲▲▲▲△△△","▲▲▲▲▲▲▲▲△△","▲▲▲▲▲▲▲▲▲△","▲▲▲▲▲▲▲▲▲▲"} },
@@ -6,18 +17,18 @@ _SYMBOL_TABLE={
 ["qq"] = { "●○", {"●○○○○","●●○○○","●●●○○","●●●●○","●●●●●","●●●●●●○○○○","●●●●●●●○○○","●●●●●●●●○○","●●●●●●●●●○","●●●●●●●●●●"} },
 }
 
-function PrintSymbol(input)
+function PrintPingji(input)
   if #input <= 0 then
     local metatables = {}
-    for k, v in pairs(_SYMBOL_TABLE) do
+    for k, v in pairs(_Pingji_TABLE) do
       table.insert(metatables, {["suggest"] = k, ["help"] = v[1]})
     end
     return metatables
-  elseif _SYMBOL_TABLE[input] then
-    return _SYMBOL_TABLE[input][2]
+  elseif _Pingji_TABLE[input] then
+    return _Pingji_TABLE[input][2]
   else
     error("Invalid argument")
   end
 end
 
-ime.register_command("pj", "PrintSymbol", "各种评级符号")
+ime.register_command("pj", "PrintPingji", "各种评级符号")
